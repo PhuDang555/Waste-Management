831fa38dfcbc735a46ca2b3db8832e6748a1d666
Dang Van Phu <phudang24012000@gmail.com>
2025-03-21 16:15:42 +0700

    thử 2

--- frontend_web/src/pages/CollectUnitAdminPage.jsx
+++ frontend_web/src/pages/CollectUnitAdminPage.jsx
@@ -46,7 +46,7 @@ const ProcessUnitAdminPage = () => {
 
   const [collectionFacilities, setCollectionFacilities] = useState([
     { id: 1, name: 'HTX MÔI TRƯỜNG GÒ VẤPdfdfdf', contact: '0989090013', address: 'Gò Vấp, TP.HCM', taxCode: '03456987' },
-    { id: 2, name: 'GRAC', contact: '0123456789', address: '', taxCode: '' },
+    { id: 2, name: 'GRACfdfsfsdf', contact: '0123456789', address: '', taxCode: '' },
     { id: 3, name: '', contact: '', address: '', taxCode: '' },
   ]);
 
